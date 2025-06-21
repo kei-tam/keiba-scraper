01baab67fa91371f5d2e2ab38c6f40eba01b3fcf
kei-tam <122681993+kei-tam@users.noreply.github.com>
2025-06-21 15:50:04 +0900

    Update keiba_scraping_oddsdata.py
    Beautifulsoup削除

--- keiba_scraping_oddsdata.py
+++ keiba_scraping_oddsdata.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import datetime
 import time
 import csv
